dd890abaeec724afca31b3a9132b50fb0e4dbd4f
Immanuella <emi.busari@gmail.com>
2025-10-23 19:29:32 +0100

    Add testing helper function

--- hello-world/hello.go
+++ hello-world/hello.go
@@ -3,8 +3,13 @@ package main
 // Why package name is not hello_world
 import "fmt"
 
+const englishHelloPrefix = "Hello, "
+
 func Hello(name string) string {
-	return "Hello, " + name
+	if name == "" {
+		name = "World"
+	}
+	return englishHelloPrefix + name
 }
 
 // The fmt.Println is a side effect (printing to stdout), and the string we send in is our domain.
--- hello-world/hello_test.go
+++ hello-world/hello_test.go
@@ -3,10 +3,34 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Chris")
-	want := "Hello, Chris"
+	t.Run("saying hello to Chris", func(t *testing.T) {
+		got := Hello("Chris")
+		want := "Hello, Chris"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	//t.Run("Say hello in Spanish", func(t *testing.T) {
+	//	got := Hello("Elly", "Spanish")
+	//	want := "Holla, Elly"
+	//
+	//	assertCorrectMessage(t, got, want)
+	//})
+}
+
+// For helper functions, it's a good idea to accept a testing.TB which is an interface that *testing.T and *testing.B
+// both satisfy, so you can call helper functions from a test, or a benchmark
+func assertCorrectMessage(t testing.TB, got, want string) {
+	t.Helper() // needed to tell the test suite that this method is a helper
 
 	if got != want {
-		t.Errorf("got %q want %q", got, want)
+		t.Errorf("got %q, want %q", got, want)
 	}
 }

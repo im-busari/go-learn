a2fd59ca074d1294b8a1ab16d7d039c16f06de93
Immanuella <emi.busari@gmail.com>
2025-10-23 19:16:47 +0100

    Hello, Chris function

--- hello-world/hello.go
+++ hello-world/hello.go
@@ -3,8 +3,12 @@ package main
 // Why package name is not hello_world
 import "fmt"
 
+func Hello(name string) string {
+	return "Hello, " + name
+}
+
 // The fmt.Println is a side effect (printing to stdout), and the string we send in is our domain.
 // What are side effects in Go?
 func main() {
-	fmt.Println("Hello World")
+	fmt.Println(Hello("world"))
 }
--- hello-world/hello_test.go
+++ hello-world/hello_test.go
@@ -3,5 +3,10 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello()
+	got := Hello("Chris")
+	want := "Hello, Chris"
+
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
 }

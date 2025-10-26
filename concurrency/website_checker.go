889589932c8ab9c27016a26fa756a0dd8208db8b
Immanuella <emi.busari@gmail.com>
2025-10-26 01:16:59 +0000

    Using goroutines and channels - concurrency

--- concurrency/website_checker.go
+++ concurrency/website_checker.go
@@ -1,13 +1,39 @@
 package concurrency
 
 type WebsiteChecker func(string) bool
+type result struct {
+	string
+	bool
+}
+
+// An operation that does not block in Go will run in a separate process called a goroutine
 
 func CheckWebsites(wc WebsiteChecker, urls []string) map[string]bool {
 	results := make(map[string]bool)
 
+	// Channels are a Go data structure that can both receive and send values. These operations,
+	// along with their details, allow communication between different processes.
+	resultChannel := make(chan result)
+
 	for _, url := range urls {
-		results[url] = wc(url)
+		// Because the only way to start a goroutine is to put go in front of a function call,
+		// we often use anonymous functions when we want to start a goroutine
+		go func() {
+			//  results[url] = wc(url)
+
+			// Send statement (<-)
+			resultChannel <- result{url, wc(url)}
+		}()
 	}
+
+	for i := 0; i < len(urls); i++ {
+		// Receive expression
+		r := <-resultChannel
+
+		results[r.string] = r.bool
+	}
+
+	//time.Sleep(2 * time.Second)
 
 	return results
 }
--- concurrency/website_checker_test.go
+++ concurrency/website_checker_test.go
@@ -3,13 +3,14 @@ package concurrency
 import (
 	"reflect"
 	"testing"
+	"time"
 )
 
 func mockWebsiteChecker(url string) bool {
 	return url != "waat://furhurterwe.geds"
 }
 
-func TestCheckWebsites(t *testing.T) {
+func TestCheckWebsitesDefault(t *testing.T) {
 	websites := []string{
 		"http://google.com",
 		"http://blog.gypsydave5.com",
@@ -28,4 +29,22 @@ func TestCheckWebsites(t *testing.T) {
 		t.Errorf("wanted %v, but got %v", want, got)
 	}
 
+}
+
+func slowStubWebsiteChecker(_ string) bool {
+	time.Sleep(20 * time.Millisecond)
+
+	return true
+}
+
+func BenchmarkCheckWebsites(b *testing.B) {
+	urls := make([]string, 100)
+
+	for i := 0; i < len(urls); i++ {
+		urls[i] = "a url"
+	}
+
+	for b.Loop() {
+		CheckWebsites(slowStubWebsiteChecker, urls)
+	}
 }

18b62c95f9dba48b4e6d1122b40894e1ba35fe66
Immanuella <emi.busari@gmail.com>
2025-10-29 22:20:38 +0000

    Using defer to close the servers

--- select/racer.go
+++ select/racer.go
@@ -8,18 +8,19 @@ import (
 // You have been asked to make a function called WebsiteRacer which takes 2 URLs and "races"
 // them by hitting them within 10 seconds then it should return an error
 func Racer(a, b string) (winner string) {
-	startA := time.Now()
-	http.Get(a)
-
-	aDuration := time.Since(startA)
-
-	startB := time.Now()
-	http.Get(b)
-	bDuration := time.Since(startB)
+	aDuration := measureResponseTime(a)
+	bDuration := measureResponseTime(b)
 
 	if aDuration < bDuration {
 		return a
 	}
 
 	return b
+}
+
+func measureResponseTime(url string) time.Duration {
+	start := time.Now()
+	http.Get(url)
+
+	return time.Since(start)
 }
--- select/racer_test.go
+++ select/racer_test.go
@@ -1,10 +1,23 @@
 package _select
 
-import "testing"
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+)
 
 func TestRacer(t *testing.T) {
-	slowURL := "http://facebook.com"
-	fastURL := "http://www.quii.dev"
+	slowServer := makeDelayedServer(20 * time.Millisecond)
+	fastServer := makeDelayedServer(0 * time.Millisecond)
+
+	// By prefixing a function call with defer it will now call that function at the end
+	// of the containing function.
+	defer slowServer.Close()
+	defer fastServer.Close()
+
+	slowURL := slowServer.URL // "http://facebook.com"
+	fastURL := fastServer.URL // "http://www.quii.dev"
 
 	want := fastURL
 	got := Racer(slowURL, fastURL)
@@ -12,4 +25,11 @@ func TestRacer(t *testing.T) {
 	if got != want {
 		t.Errorf("got %q, want %q", got, want)
 	}
+}
+
+func makeDelayedServer(delay time.Duration) *httptest.Server {
+	return httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(delay)
+		w.WriteHeader(http.StatusOK)
+	}))
 }

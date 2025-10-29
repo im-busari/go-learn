cfa72c04f1ca5545d21a7ac844374202fd351dd8
Immanuella <emi.busari@gmail.com>
2025-10-29 22:34:18 +0000

    Craeting configurableRacer to make logic easier to test

--- select/racer.go
+++ select/racer.go
@@ -1,9 +1,43 @@
 package _select
 
 import (
+	"fmt"
 	"net/http"
 	"time"
 )
+
+var tenSecondTimeout = 10 * time.Second
+
+func RacerWithSelect(a, b string) (winner string, error error) {
+	return ConfigurableRacer(a, b, tenSecondTimeout)
+}
+
+func ConfigurableRacer(a, b string, timeout time.Duration) (winner string, error error) {
+	// select allows you to wait on multiple channels. The first one to send a value "wins"
+	// and the code underneath the case is executed.
+	select {
+	case <-ping(a):
+		return a, nil
+	case <-ping(b):
+		return b, nil
+	case <-time.After(timeout):
+		return "", fmt.Errorf("timed out waiting for %s and %s", a, b)
+	}
+}
+
+func ping(url string) chan struct{} {
+	// Notice how we have to use make when creating a channel; rather than say var ch chan struct{}. When you use var the
+	// variable will be initialised with the "zero" value of the type. So for string it is "", int it is 0, etc.
+	// For channels the zero value is nil and if you try and send to it with <- it will block forever because you cannot send to nil channels
+	channel := make(chan struct{})
+	go func() {
+		http.Get(url)
+		close(channel)
+	}()
+
+	return channel
+
+}
 
 // You have been asked to make a function called WebsiteRacer which takes 2 URLs and "races"
 // them by hitting them within 10 seconds then it should return an error
--- select/racer_test.go
+++ select/racer_test.go
@@ -8,23 +8,39 @@ import (
 )
 
 func TestRacer(t *testing.T) {
-	slowServer := makeDelayedServer(20 * time.Millisecond)
-	fastServer := makeDelayedServer(0 * time.Millisecond)
+	t.Run("compares speeds of servers, returning the URL of the dfastest one", func(t *testing.T) {
+		slowServer := makeDelayedServer(20 * time.Millisecond)
+		fastServer := makeDelayedServer(0 * time.Millisecond)
 
-	// By prefixing a function call with defer it will now call that function at the end
-	// of the containing function.
-	defer slowServer.Close()
-	defer fastServer.Close()
+		// By prefixing a function call with defer it will now call that function at the end
+		// of the containing function.
+		defer slowServer.Close()
+		defer fastServer.Close()
 
-	slowURL := slowServer.URL // "http://facebook.com"
-	fastURL := fastServer.URL // "http://www.quii.dev"
+		slowURL := slowServer.URL // "http://facebook.com"
+		fastURL := fastServer.URL // "http://www.quii.dev"
 
-	want := fastURL
-	got := Racer(slowURL, fastURL)
+		want := fastURL
+		got, _ := RacerWithSelect(slowURL, fastURL)
 
-	if got != want {
-		t.Errorf("got %q, want %q", got, want)
-	}
+		if got != want {
+			t.Errorf("got %q, want %q", got, want)
+		}
+	})
+
+	t.Run("returns an error if a server doesn't respond within 10s", func(t *testing.T) {
+		serverA := makeDelayedServer(25 * time.Millisecond)
+		serverB := makeDelayedServer(25 * time.Millisecond)
+
+		defer serverB.Close()
+		defer serverA.Close()
+
+		_, err := ConfigurableRacer(serverA.URL, serverB.URL, 20*time.Millisecond)
+
+		if err == nil {
+			t.Error("expected an erro but didn't get one")
+		}
+	})
 }
 
 func makeDelayedServer(delay time.Duration) *httptest.Server {
